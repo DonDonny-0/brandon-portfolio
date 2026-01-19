4c5eb1b8f92d9ca34ad711496f02000d11d65a98
Brandon <brand.t2004@icloud.com>
2026-01-19 15:18:22 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,119 @@
+$(document).ready(function() {
+
+  // jquery for toggle sub menu
+  $('.sub-btn').click(function() {
+    $(this).next('.sub-menu').slideToggle();
+    $(this).find('.dropdown').toggleClass('rotate');
+  })
+
+  // jquery for expand and collapse the sidebar
+  $('.menu-btn').click(function() {
+    $('.side-bar').addClass('active');
+    $('.menu-btn').css('display', 'none');
+  });
+
+  $('.close-btn').click(function() {
+    $('.side-bar').removeClass('active');
+    $('.menu-btn').css('display', 'block');
+  })
+
+  $(document).ready(function() {
+    $('.intro').animsition({
+      inClass: 'fade-in-down-sm',
+      outDuration: 1000
+    });
+  });
+
+  let lastScrollPos = 0;
+
+  window.onscroll = () => {stickyHeader()}
+
+  function stickyHeader() {
+    const currentScrollPos = window.pageYOffset;
+
+    if (currentScrollPos > 200) {
+      if (currentScrollPos < lastScrollPos) {
+        $('.header').css('visibility', 'visible').stop(true).animate({top: '0'});
+      }
+      else {
+        $('.header').stop(true).animate({top: '-100px'});
+      }
+    }
+    else {
+      if (currentScrollPos === 0) {
+        $('.header').stop(true).animate({top: '-100px'});
+      }
+    }
+
+    lastScrollPos = currentScrollPos;
+  }
+
+})
+
+window.addEventListener('scroll', () => {
+  document.querySelector('.fa-bars').classList.toggle('scrolled', window.scrollY > 800);
+});
+
+const form = document.getElementById('form');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email');
+const subject = document.getElementById('subject');
+const message = document.getElementById('message');
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+
+//   validateInputs();
+// });
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+  const re = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+  return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+  const firstNameVal = firstName.value.trim();
+  const lastNameVal = lastName.value.trim();
+  const emailVal = email.value.trim();
+
+  if (firstNameVal === '') {
+    setError(firstName, 'First Name is Required');
+  } else {
+    setSuccess(firstName);
+  }
+
+  if (lastNameVal === '') {
+    setError(lastName, 'Last Name is Required');
+  } else {
+    setSuccess(lastName)
+  }
+
+  if (emailVal === '') {
+    setError(email, 'Email is Required');
+  } else if (!isValidEmail(emailVal)) {
+    setError(email, 'Provide a valid email address');
+  } else {
+    setSuccess(email);
+  }
+
+
+}
